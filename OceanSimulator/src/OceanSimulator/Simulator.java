7217f898e1af682dab45a7288a4ac12ec2ede920
Cheung Yan Pui <180127003@aston.ac.uk>
2019-11-20 01:19:05 +0000

    Progress until lab5.

--- OceanSimulator/src/OceanSimulator/Simulator.java
+++ OceanSimulator/src/OceanSimulator/Simulator.java
@@ -4,57 +4,77 @@
  * and open the template in the editor.
  */
 package OceanSimulator;
+
 import java.awt.*;
+import java.util.Random;
 
 /**
  *
  * @author 180127003
  */
 public class Simulator {
-    
+
     /**
      *
-     * Initialise creatures, field and its view.  
-     * They are created once here and passed to the parts that
-     * need them so there is only one copy of each.
+     * Initialise creatures, field and its view. They are created once here and
+     * passed to the parts that need them so there is only one copy of each.
      */
     private static SimulatorView simulatorView;
     private static Field field;
 
-    
     private Plankton plankton;
     private Sardine sardine;
     private Shark shark;
-    
-        private Simulator(int row, int col){
-        field  = new Field(row, col);
+    private Random rand;
+
+    public static final double PLANKTON_CREATE_PROB = 0.3;
+    public static final double SARDINE_CREATE_PROB = 0.4;
+    public static final double SHARK_CREATE_PROB = 0.2;
+
+    private Simulator(int row, int col) {
+        RandomGenerator.initialiseWithSeed(4);
+        rand = RandomGenerator.getRandom();
+        field = new Field(row, col);
         simulatorView = new SimulatorView(row, col);
         simulatorView.setColor(Plankton.class, Color.green);
         simulatorView.setColor(Sardine.class, Color.orange);
         simulatorView.setColor(Shark.class, Color.red);
 
     }
-    
-    
-    public static void main(String[] args){
-        Simulator simulator = new Simulator(50,50);
+
+    public static void main(String[] args) {
+        Simulator simulator = new Simulator(50, 50);
         simulator.startSimulaion();
     }
 
-    
-    private void populate(){
-        plankton = new Plankton();
-        sardine = new Sardine();
-        shark = new Shark();
-        
-        field.place(plankton, 10, 10);
-        field.place(sardine, 20, 20);
-        field.place(shark, 30, 30);
+    /**
+     * populate the field. Notice the probability must go from low to high, if
+     * using else if structure.
+     *
+     */
+    private void populate() {
+        field.clear();
+        for (int i = 0; i < field.getDepth(); i++) {
+            for (int j = 0; j < field.getWidth(); j++) {
+                double probability = rand.nextDouble();
+                 if (probability < SHARK_CREATE_PROB) {
+                    field.place(new Shark(), i, j);
+                } 
+                else if (probability < PLANKTON_CREATE_PROB) {
+                    field.place(new Plankton(), i, j);
+                } 
+                else if (probability < SARDINE_CREATE_PROB) {
+                    field.place(new Sardine(), i, j);
+                } else {
+                }
+            }
+        }
+
     }
-    
-    private void startSimulaion(){
+
+    private void startSimulaion() {
         populate();
         simulatorView.showStatus(0, field);
     }
-    
+
 }
