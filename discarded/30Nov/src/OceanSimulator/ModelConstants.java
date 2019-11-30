63e83864ced27c48e735f99df47a4ca579d54a14
Cheung Yan Pui <180127003@aston.ac.uk>
2019-11-30 20:24:17 +0000

    Restructured the projects, all duplicated copies moved to discard folder. Latest copy is in the main folder.

---
+++ discarded/30Nov/src/OceanSimulator/ModelConstants.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OceanSimulator;
+
+/**
+ *
+ * @author 180127003
+ */
+public class ModelConstants {
+     //declearing the default depth of the Ocean field.
+    public static final int DEFAULT_DEPTH = 50; 
+    //declearing the default width of the Ocean field.
+    public static final int DEFAULT_WIDTH = 50;
+    public static final double SIM_LENGTH = 1000;
+    
+       //declearing the plankton generating probability 
+    public static final double PLANKTON_CREATE_PROB = 0.3;
+    //declearing the srdine generating probability 
+    public static final double SARDINE_CREATE_PROB = 0.4;
+    //declearing the shark generating probability 
+    public static final double SHARK_CREATE_PROB = 0.2;
+}
+     
